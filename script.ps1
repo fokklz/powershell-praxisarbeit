8ea6970625b0a982561cda28cfc7f5c4f3c46446
Fokko Vos <fokklzofficial@gmail.com>
2023-06-11 20:26:48 +0200

    Working v1

--- script.ps1
+++ script.ps1
@@ -47,13 +47,17 @@ param(
     [switch]$UseContent, # enable modification date detection by file content
     [switch]$MapOnly, # only map projects, do not move them
     [switch]$Interactive, # enable manual primary project selection
-    [switch]$Flat # keep all projects in root when moving
+    [switch]$Flat, # keep all projects in root when moving
+    [switch]$MakeLonger # make the script take longer
 )
+
+$ErrorActionPreference = "Stop"
 
 # =====================================================================
 # Variables
 # =====================================================================
 
+# original working directory
 $OG_PWD = $PWD
 # temporary log array, to reduce file access
 $LOG_TEMP = New-Object -TypeName System.Collections.ArrayList
@@ -74,6 +78,11 @@ $PROJECT_FILES = @(
     "README.md" 
 )
 
+$STARTED = Get-Date
+
+$COUNT_PROJECT_VERSIONS = 0
+$COUNT_PROJECTS_MOVED = 0
+
 # test if path is absolute & exists
 # regex: https://regex101.com/r/OVptX3/3
 $RGX_ABSOLUTE_PATH = '^(?:(?:[A-Za-z]:)?(?:\.)?[\\/]){1,}?.*'
@@ -89,15 +98,52 @@ $RGX_COPYRIGHT = '[Cc]opyright.*(\d{4})'
 # Functions
 # =====================================================================
 
-# may not start with highlighted part
-# same %s as additional parameter
+function Get-RunTime {
+    <#
+    .SYNOPSIS
+    Returns the runtime of the script.
+
+    .DESCRIPTION
+    Returns the runtime of the script.
+
+    .NOTES
+    The script should only be ended with this function. in case of an error.
+    #>
+    $runTime = ((Get-Date) - $STARTED)
+    return "00:{0:00}:{1:00}" -f $runTime.Minutes, $runTime.Seconds
+}
 
 function Exit-Error {
-    # reset working directory
+    <#
+    .SYNOPSIS
+    Finishes the script with an error.
+
+    .DESCRIPTION
+    Finishes the script with an error. Resets the working directory and writes the error to the log file.
+
+    .NOTES
+    The script should only be ended with this function. in case of an error.
+    #>
     Set-Location $OG_PWD
     Write-Out -Type "ERROR" $args.Exception -ForceWriteLog -LogOnly
     exit 1
 }   
+
+function Exit-Success {
+    <#
+    .SYNOPSIS
+    Finishes the script successfully.
+    
+    .DESCRIPTION
+    Finishes the script successfully. Resets the working directory and writes a final info message to the log file.
+    
+    .NOTES
+    The script should only be ended with this function in case of success.
+    #>
+    Set-Location $OG_PWD
+    Write-Out -Type "INFO" "Skript wurde erfolgreich durchgeführt, Vorgang hat %s gedauert" (Get-RunTime) -ForceWriteLog
+    exit 0
+}
 
 function Write-Out {
     param(
@@ -106,10 +152,10 @@ function Write-Out {
         [string]$Type = "INFO",
         [switch]$LogOnly,
         [switch]$ForceWriteLog,
+        [switch]$AddNewLine,
         [Parameter(ValueFromRemainingArguments = $true)]
-        $highlighted
+        $highlighted # replacements for %s
     )
-
 
     $fullMessage = $Message
     $printed = $false
@@ -155,13 +201,23 @@ function Write-Out {
                 }
             }
         }
+
+        if ($AddNewLine) {
+            Write-Host ""
+        }
     }
 
     $LOG_TEMP.Add("$(Get-Date -Format "dd-MM-yyyy HH:mm:ss") [$Type] $fullMessage") | Out-Null
 
     if ($LOG_TEMP.Count -gt 30 -or $ForceWriteLog) {
-        $LOG_TEMP | Out-File -FilePath $LOG_FILE -Append
-        $LOG_TEMP.Clear()
+        try {
+            $LOG_TEMP | Out-File -FilePath $LOG_FILE -Append -Encoding utf8
+        } catch {
+            Write-Host "Fehler beim Schreiben in die Log-Datei" -ForegroundColor Red
+            Write-Host $_.Exception.Message -ForegroundColor Red
+        } finally {
+            $LOG_TEMP.Clear()
+        }
     }
 }
 
@@ -257,14 +313,16 @@ function Find-Projects {
         [string]$Path
     )
 
-    # read trough folder, call self on all subfolders
+    # read trough folder
+    # exit on error
     Get-ChildItem -Path $Path -Directory | Where-Object {
         $_.FullName -notmatch $BLACKLIST
     } | Select-Object FullName | ForEach-Object {
         if (-not (Test-Project -Path $_.FullName)) {
-            # to much?, Write-Out "Verzeichnis %s wird gelesen..." (Resolve-Path -Path $_.FullName -Relative)
+            # call self on subfolder
             Find-Projects -Path $_.FullName
         } else {
+            # get project identifyer (name or hash)
             $hash = Get-Hash $_.FullName
 
             # create project index, if not existing
@@ -276,13 +334,14 @@ function Find-Projects {
                 path    = $_.FullName
                 date    = (Get-LastModificationDate $_.FullName)
                 primary = $false
+                newPath = $null
             }
 
-            # add date and path
+            # store data
             [void]$INDEX[$hash].Add($data)
             
             Write-Out "Projekt %s gefunden" $hash
-            Write-Out "  - %s`n" (Resolve-Path -Path $_.FullName -Relative)
+            Write-Out "  - %s" (Resolve-Path -Path $_.FullName -Relative) -AddNewLine
         }
         
     }
@@ -290,11 +349,26 @@ function Find-Projects {
 
 
 function Test-Project {
+    <#
+    .SYNOPSIS
+    Test if is a project-folder
+    
+    .DESCRIPTION
+    Test if a project file exists in the given path
+    based on the PROJECT_FILES array defined
+    
+    .PARAMETER Path
+    Existing folder path (possible project folder)
+    
+    .EXAMPLE
+    Test-Project -Path $Path
+    #>
     param(
         [Parameter(Mandatory = $true, Position = 0)]
         [string]$Path
     )
 
+    # test for each project file
     foreach ($projectFile in $PROJECT_FILES) {
         if (Test-Path -Path (Join-Path -Path $Path -ChildPath $projectFile)) {
             return $true
@@ -305,18 +379,126 @@ function Test-Project {
 }
 
 function Write-ProjectsJSON {
+    <#
+    .SYNOPSIS
+    Write projects to json file
+    
+    .DESCRIPTION
+    convert the data for the Projects to a valid json format
+    and write the file to the disk
+    
+    .PARAMETER HashTable
+    Hashtable containing the projects
+    
+    .EXAMPLE
+    Write-ProjectsJSON -HashTable $INDEX
+    
+    .NOTES
+    The date is converted to a universal time format
+    #>
+
+    # formatted data for json
     $storableProjects = New-Object -TypeName System.Collections.Hashtable
+
     foreach ($key in $INDEX.Keys) {
+        # clone to avoid mutation errors
         $values = $INDEX[$key] | ForEach-Object { $_ }
         for ($i = 0; $i -lt $values.Count; $i++) {
             $values[$i].date = $values[$i].date.ToString("yyyy-MM-ddTHH:mm:ssZ")
+            if ($MapOnly) {
+                # remove new path when only mapping
+                $newData = [PSCustomObject]@{
+                    path    = $values[$i].path
+                    date    = $values[$i].date
+                    primary = $values[$i].primary
+                }
+                $values[$i] = $newData
+            }
         }
         $storableProjects.Add($key, $values)
     }
+
+    # write to file
     $storableProjects | ConvertTo-Json -Depth 2 | Out-File -FilePath $PROJECTS_FILE -Encoding UTF8
+    Write-Out "Projetinformationen als JSON in %s geschpeichert" $PROJECTS_FILE
 }
 
-# =====================================================================
+function Update-Progress {
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$name
+    )
+
+    if ($COUNT_PROJECTS_MOVED -gt 0) {
+        $complete = ($COUNT_PROJECTS_MOVED / $COUNT_PROJECT_VERSIONS) * 100
+        if ($COUNT_PROJECTS_MOVED -eq $COUNT_PROJECT_VERSIONS -or $complete -ge 100) {
+            Write-Progress -Id 1 -Activity "Projekte werden verschoben" -Status "Vollendet" -PercentComplete 100 -Completed
+            return
+        }
+        Write-Progress -Id 1 -Activity "Projekte werden verschoben" -Status "$name wird verschoben..." -PercentComplete $complete
+    }
+}
+
+function Move-WithProgress {
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [PSCustomObject]$Data,
+        [Parameter(Mandatory = $false, Position = 1)]
+        [PSCustomObject]$Primary = $null,
+        [int]$Version = 0,
+        [boolean]$Flat
+    )
+
+    $COUNT_PROJECTS_MOVED++
+    Move-Project -Data $Data -Primary $Primary -Version $Version -Flat $Flat
+}
+
+function Move-Project {
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [PSCustomObject]$Data,
+        [Parameter(Mandatory = $false, Position = 1)]
+        [PSCustomObject]$Primary = $null,
+        [int]$Version = 0,
+        [boolean]$Flat
+    )
+
+    $year = $Data.date.Year
+    $name = Split-Path $Data.path -Leaf
+    $newPath = $null
+    
+    if ($Primary -ne $null -and $Version -gt 0) {
+        $primaryName = Split-Path $Primary.path -Leaf
+        $name = "$primaryName/.versions/v$($Version)_$name"
+    }
+    
+    Update-Progress $name
+
+    if ($Flat) {
+        $newPath = Join-Path $Out $name
+    } else {
+        $newPath = Join-Path $Out (Join-Path $year $name)
+    }
+
+    try {
+        New-Item -Path (Split-Path $newPath -Parent) -ItemType Directory -Force | Out-Null
+        Move-Item -Path $Data.path -Destination $newPath -Force
+        $script:COUNT_PROJECTS_MOVED += 1
+        Write-Out -Type "INFO" "Das Projekt %s wurde verschoben (%s/%s)" $name $COUNT_PROJECTS_MOVED $COUNT_PROJECT_VERSIONS
+        Update-Progress $name
+    } catch {
+        Write-Out -Type "ERROR" "Das Projekt %s konnte nicht verschoben werden" $name
+        Write-Out -Type "ERROR" $_.Exception -LogOnly
+    } finally {
+        # allow for some delay while shoing in presentation
+        if ($MakeLonger) {
+            Start-Sleep -Seconds 1
+        }
+    }
+
+    return $newPath
+}
+# =====================================================================-
 # Validation
 # =====================================================================
 
@@ -325,7 +507,7 @@ if ([string]::IsNullOrEmpty($Path)) {
     Write-Out -Type "ERROR" "Der Parmaeter '-Path' darf nicht leer sein"
     Exit-Error
 }
-if ([string]::IsNullOrEmpty($Path)) {
+if ([string]::IsNullOrEmpty($Out)) {
     Write-Out -Type "ERROR" "Der Parmaeter '-Out' darf nicht leer sein"
     Exit-Error
 }
@@ -353,12 +535,12 @@ if ($Out -notmatch $RGX_ABSOLUTE_PATH) {
 }
 # ensure out-path is syntactically correct
 try {
-    New-Item -Path $Out -ItemType Directory -Force -ErrorAction Stop | Out-Null
+    New-Item -Path $Out -ItemType Directory -Force | Out-Null
     if ((Get-ChildItem -Path $Out).Length -gt 0) {
         # help user to not overwrite existing files if not desired
         if ($Force) {
             Write-Out -Type "WARN" "Der angegebene Out-Pfad %s existiert bereits und wird überschrieben" $Out
-            Remove-Item -Path "$Out/*" -Recurse -Force
+            Remove-Item -Path "$Out/*" -Recurse -Force -ErrorAction SilentlyContinue
         } else {
             Write-Out -Type "ERROR" "Der angegebene Out-Pfad %s existiert bereits" $Out
             Exit-Error
@@ -375,14 +557,13 @@ Set-Location $Path
 # Variables Dependent on Parameters
 # =====================================================================
 
-$LOG_FILE = Join-Path -Path $Out -ChildPath "cleanup.log"
-$PROJECTS_FILE = Join-Path -Path $Out -ChildPath "projects.json"
+$LOG_FILE = Join-Path $Out "cleanup.log"
+$PROJECTS_FILE = Join-Path $Out "projects.json"
 
 # =====================================================================
 # Script
 # =====================================================================
 
-Write-Out -Type "SYSTEM" "Starte Ausführung von Cleanup-Skript" -LogOnly
 Write-Out "Aufräumen von %s nach %s" $Path $Out -ForceWriteLog
 
 try {
@@ -390,13 +571,10 @@ try {
 
     # clone keys
     $keys = $INDEX.Keys | ForEach-Object { $_ }
-    $totalProjectCount = 0
     # set primary projects
     foreach ($key in $keys) {
         $sorted = $INDEX[$key] | Sort-Object -Property date -Descending
         $primaryIndex = $null
-        # calculate total count
-        $totalProjectCount += $sorted.Count
         
         if ($Interactive -and $sorted.Count -gt 1) {
             Write-Host "`n"
@@ -413,7 +591,7 @@ try {
             while ($selectedIndex -lt 1 -or $selectedIndex -ge ($sorted.Count + 1)) {
                 $input = Read-Host -Prompt "Bitte wähle die primäre Version aus (1)"
 
-                if ($input -eq "" -or $input -eq $null) {
+                if ([string]::IsNullOrEmpty($input)) {
                     # set default if no value provided
                     $selectedIndex = 1
                     break
@@ -440,19 +618,64 @@ try {
     }
 
     if ($INDEX.Count -gt 0) {
+        $INDEX.Keys | ForEach-Object {
+            $COUNT_PROJECT_VERSIONS += $INDEX[$_].Count
+        }
         # write findings summary
-        Write-Out "`n`n%s Versionen, %s Projekte gefunden" $totalProjectCount $INDEX.Count
+        Write-Host "`n"
+        Write-Out " %s Versionen, %s Projekte gefunden" $COUNT_PROJECT_VERSIONS $INDEX.Count
         if ($MapOnly) {
             Write-ProjectsJSON
-            Write-Out "Die Projekte wurden als JSON in %s gespeichert`n" $PROJECTS_FILE -ForceWriteLog
-            exit 0
+            Write-Out "Die Projekte wurden als JSON in %s gespeichert" $PROJECTS_FILE -ForceWriteLog
+            Exit-Success
         } else {
-            
+            $useFlat = $false
+            if ($Interactive -and -not $Flat) {
+                $inputString = $null
+                while ($inputString -ne "y" -and $inputString -ne "n") {
+                    $inputString = Read-Host -Prompt "Möchtest du die Projekte nach Datum sortiert abglegen? (y/n, default: y)"
+
+                    if ([string]::IsNullOrEmpty($inputString)) {
+                        break
+                    }
+
+                    if ($inputString -eq "y") {
+                        break
+                    } elseif ($inputString -eq "n") {
+                        $useFlat = $true
+                    } else {
+                        Write-Out -Type "ERROR" "Eingabe %s ungültig. Bitte gib 'y' oder 'n' ein." $inputString
+                    }
+                }
+            } elseif ($Flat) {
+                $useFlat = $false
+            }
+
+            Write-Out "Die Projekte werden nach %s verschoben" $Out -ForceWriteLog -AddNewLine
+
+            # cloned to avoid mutating while iterating
+            $keys = $INDEX.Keys | ForEach-Object { $_ }
+            $keys | ForEach-Object {
+                $projects = $INDEX[$_] | Sort-Object -Property primary -Descending
+                $primary = $projects[0]
+                $movedPrimary = Move-Project $primary -Flat $useFlat
+                $primary.newPath = $movedPrimary
+                for ($i = 1; $i -lt $projects.Count; $i++) {
+                    $movedVersion = Move-Project $projects[$i] $primary -Version $i -Flat $useFlat
+                    $projects[$i].newPath = $movedVersion
+                }
+                $INDEX[$_] = $projects
+            }
+            Write-ProjectsJSON
+
+            Write-Host "`n"
+            Write-Out "Die Projekte wurden erfolgreich verschoben"
+            Exit-Success
         }
     }
         
-    Write-Out -Type "WARN" "Keine Projekte gefunden`n" -ForceWriteLog
-    exit 0
+    Write-Out -Type "WARN" "Keine Projekte gefunden"
+    Exit-Success
 } catch {
     Write-Out -Type "ERROR" "Fehler beim Aufräumen von %s" $Path
     Write-Out -Type "ERROR" $_.Exception.Message
--- scripts/setup-test.ps1
+++ scripts/setup-test.ps1
@@ -1,7 +1,1 @@
-﻿if(-not (Test-Path (Join-Path $PWD 'test-data'))) {
-    git clone https://github.com/FokklzBulk/powershell-praxisarbeit-test.git test-data
-}else{
-    Write-Host "Zurücksetzen: test-data. Dies könnte einen Moment dauern..."
-    git -C $PWD/test-data checkout -- . | Out-Null
-}
-
+﻿git clone https://github.com/FokklzBulk/powershell-praxisarbeit-test.git test-data
